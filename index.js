a87af880323326d825f7450846464d2e89355221
Dustin Specker <dustinspecker@dustinspecker.com>
2015-10-20 17:36:07 -0500

    refactor(index): remove useless variable

--- index.js
+++ index.js
@@ -4,13 +4,11 @@ var cheerio = require('cheerio')
 
 module.exports = function () {
   return got('http://en.wikipedia.org/wiki/Special:Random').then(function (response) {
-    var $, title, topic;
+    var $, title;
 
     $ = cheerio.load(response.body);
     // use title to prevent dealing with formatting (<i></i>)
     title = $('title').text();
-    topic = title.substr(0, title.indexOf(' - Wikipedia, the free encyclopedia'));
-
-    return topic;
+    return title.substr(0, title.indexOf(' - Wikipedia, the free encyclopedia'));
   });
 };
